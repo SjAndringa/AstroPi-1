54dd16b1e8ce8e51d431e986f778e647f737424c
SjAndringa <sjoerd.andringa@gmail.com>
2026-02-08 21:59:48 +0100

    better G

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ from sense_hat import SenseHat
 import numpy as np
 from scipy.optimize import fsolve
 
-R = 6.7885E6 # radius earth + height ESA
+R_earth = (12756+12714)/4*1E3 # mean diameter earth / 4 is mean radius earth
+H_esa = 421E3   #height esa, but it fluctuates
+
+R = R_earth+H_esa # radius earth + height ESA
 OMEGA_0 = 0.00116 # initial estimate of angle velocity
 
 V_0 = OMEGA_0*R #initial estimate of velocity, in m/s
 
-G=9.80665 #Gforce, used to recalculate accelerometer from Gs to m/s²
+G_0=9.80665 #Gforce, used to recalculate accelerometer from Gs to m/s²
             # maybe G isn't that value at that height
+G=G_0*(R_earth/R_earth+H_esa)**2
 
 SampleInterval=10 #wait time between two acceleration samples
-SampleTime = 8*60*1E9   #total time to gather information 8 minutes
+SampleTime = 1*60*1E9   #total time to gather information 8 minutes
 
 def formula(x,a,R,t0,t1):
     
